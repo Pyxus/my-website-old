dfe8c3bdf71bd1c4bd575170fd2318284c77596e
pyxus <eze.halley@gmail.com>
2023-11-27 04:14:46 -0500

    Add new src/lib constants

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,5 @@
-// place files you want to import through the `$lib` alias in this folder.
+import { dev } from "$app/environment"
+
+export const title = 'My Blog'
+export const description = 'SvelteKit blog'
+export const url = dev ? 'http://localhost:5173/' : 'https://ezekiel.page'
